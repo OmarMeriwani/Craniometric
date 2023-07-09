6e724a689cad529c1d0d0c309fb334bf9b212e2f
Omar Meriwani <omar.sirwan@gmail.com>
2023-07-09 13:26:15 +0100

    --

--- model.py
+++ model.py
@@ -2,13 +2,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
-df = pd.read_excel("E:/مقالات علمية/Howell.xlsx","Sheet1")
+sex = 0
+df = pd.read_excel("Howell.xlsx","Sheet1")
 labels_sex = [d[2] for d in df.values]
-labels_group = [d[3] for d in df.values]
-x = [d[5:] for d in df.values]
+labels_group = [d[3] for d in df.values if d[2] == 2]
+x=[]
+if sex == 0:
+    x = [d[5:] for d in df.values if d[2] == 2]
+else:
+    x = [d[5:] for d in df.values ]
 x = [y.tolist() for y in x]
-x_train, x_test, y_train, y_test = train_test_split(x, labels_group, test_size=0.2)
-model = MLPClassifier(hidden_layer_sizes=(120,160,180,120))
+if sex == 0:
+    x_train, x_test, y_train, y_test = train_test_split(x, labels_group, test_size=0.2)
+else:
+    x_train, x_test, y_train, y_test = train_test_split(x, labels_sex, test_size=0.2)
+
+model = MLPClassifier(hidden_layer_sizes=(120,160,180))
 model.fit(x_train,y_train)
 score = model.score(x_test,y_test)
 print(score)
